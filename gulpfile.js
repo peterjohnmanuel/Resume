ae0af78e3778abde4207b2bfc397a8c5f0d59497
Peter John Manuel <peterjohn.manuel@gmail.com>
2016-03-22 01:39:04 +0200

    Initial commit of work done so far.

---
+++ gulpfile.js
@@ -0,0 +1,153 @@
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    minifyCSS = require('gulp-clean-css'),
+    concat = require('gulp-concat'),
+    plumber = require('gulp-plumber'),
+    coffee = require('gulp-coffee'),
+    useref = require('gulp-useref'),
+    livereload = require('gulp-livereload'),
+    gulpIf = require('gulp-if')
+    htmlmin = require('gulp-htmlmin'),
+    responsive = require('gulp-responsive-images');
+
+
+livereload({ start: true });
+
+
+// gulp.task('scripts', function() {
+//     gulp.src('js/**/*.js')
+//         .pipe(plumber())
+//         .pipe(coffee())
+//         .pipe(concat('app.min.js'))
+//         .pipe(uglify())
+//         .pipe(gulp.dest('dist/js/'))
+//         .pipe(livereload());
+// });
+
+
+// gulp.task('styles', function() {
+//     gulp.src('css/**/*.css')
+//         .pipe(plumber())
+//         .pipe(coffee())
+//         .pipe(concat('main.min.css'))
+//         .pipe(minifyCSS())
+//         .pipe(gulp.dest('dist/css/'))
+//         .pipe(livereload());
+// });
+
+gulp.task('fonts', function() {
+    gulp.src('fonts/**/*')
+        .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('images', function() {
+    gulp.src('images/**/*')
+        .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('useref', function() {
+    gulp.src('*.html')
+        .pipe(useref())
+        .pipe(gulpIf('*.js', uglify()))
+        .pipe(gulpIf('*.css', minifyCSS()))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('htmlmin', function() {
+    gulp.src('dist/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('responsive', function() {
+    gulp.src('images/**/*')
+        .pipe(responsive({
+
+            '*.jpg': [
+                {
+                    width: 1600,
+                    suffix: '-XL',
+                    quality: 100
+                },
+                {
+                    width: 1200,
+                    suffix: '-L',
+                    quality: 100
+                },
+                {
+                    width: 800,
+                    suffix: '-M',
+                    quality: 100
+                },
+                {
+                    width: 400,
+                    suffix: '-S',
+                    quality: 40
+                },
+                {
+                    width: 40,
+                    suffix: '-Logo',
+                    quality: 100
+                },
+                {
+                    width: 758,
+                    height: 60,
+                    suffix: '-Banner',
+                    quality: 100
+                }
+
+            ],
+
+            '*.png': [
+                {
+                    width: 1600,
+                    suffix: '-XL',
+                    quality: 100
+                },
+                {
+                    width: 1200,
+                    suffix: '-L',
+                    quality: 100
+                },
+                {
+                    width: 800,
+                    suffix: '-M',
+                    quality: 100
+                },
+                {
+                    width: 400,
+                    suffix: '-S',
+                    quality: 40
+                },
+                {
+                    width: 40,
+                    suffix: '-Logo',
+                    quality: 100
+                },
+                {
+                    width: 758,
+                    height: 60,
+                    suffix: '-Banner',
+                    quality: 100
+                }]
+
+        }))
+        .pipe(gulp.dest('dist/images'));
+
+});
+
+gulp.task('watch', function() {
+    
+    livereload.listen();
+    
+    gulp.watch('js/**/*.js', ['useref']);
+    gulp.watch('css/**/*.css', ['useref']);
+    gulp.watch('images/**/*', ['responsive']);
+    gulp.watch('fonts/**/*', ['fonts']);
+    gulp.watch('*.html', ['htmlmin']);
+});
+
+//gulp.task('default', ['fonts', 'useref', 'htmlmin', 'responsive', 'images','watch']);
+
+gulp.task('default', ['fonts', 'useref', 'responsive', 'images','watch']);
---
+++ js/resumeBuilder.js
@@ -0,0 +1,249 @@
+// Bio
+var bio = {
+    "name": "Peter John Manuel",
+    "role": "Software Developer",
+    "contacts": {
+        "email": "peterjohn.manuel@yahoo.com",
+        "github": "https://github.com/peterjohnmanuel",
+        "location": "Cape Town, South Africa"
+    },
+    "welcomeMessage": "lorem ipsum dolor sit amet etc etc etc",
+    "skills": ["awesomness", "delivering things", "cryogenic sleep", "saving the universe"],
+    "bioPic": "images/fry.jpg"
+};
+
+// Work
+var work = {
+
+    "jobs": [
+        {
+            "employer": "Process Computer Technology",
+            "title": "Intership",
+            "dates": "July 2012 - December 2012",
+            "location": "Bloubergstrand, Cape Town, South Africa",
+            "description": "I started my career at PCT as an intern during the work integrated learning program at CPUT. During this time I was on a six month contract to learn about industry and after the six month contract was promoted to junior developer. PCT uses a scrum/agile methodology integrated with a Jira tracking and ticketing system to monitor progress. During my intership at PCT I was involved with the following projects:",
+        },
+        {
+            "employer": "Process Computer Technology",
+            "title": "Junior Developer",
+            "dates": "January 2013 - November 2015",
+            "location": "Bloubergstrand, Cape Town, South Africa",
+            "description": "After my 6 month contract as an intern at PCT, I was employed as a Junior Developer and was involved with the following projects:",
+            "projects": []
+        },
+        {
+            "employer": "Red Panda Software",
+            "title": "Software Developer",
+            "dates": "December 2015 - Current ",
+            "location": "Pinelands, Cape Town, South Africa",
+            "description": "description"
+        }
+    ],
+};
+
+var education = {
+
+    "schools":
+    [
+        {
+            "name": "Oude Molen Technical High School",
+            "location": "Pinelands, Cape Town, South Africa, ",
+            "qualification": "National Senior Certificate",
+            "year": "2009",
+            "major": "Electrical Technology",
+            "schoolPic": "images/education/oudemolen_logo.jpg"
+        },
+        {
+            "name": "Cape Peninsula University of Technology",
+            "location": "Cape Town, Cape Town, South Africa",
+            "qualification": "NDip IT: Nation Diploma Information Technology",
+            "year": "2012",
+            "major": "Software Development",
+            "schoolPic": "images/education/CPUT_logo.jpg"
+        },
+        {
+            "name": "Cape Peninsula University of Technology",
+            "location": "Cape Town, Cape Town, South Africa",
+            "qualification": "BTech IT: Baccules Technology Information Technology",
+            "year": "2014",
+            "major": "Software Development",
+            "schoolPic": "images/education/CPUT_logo.jpg"
+        }
+    ],
+
+    "onlineCourses": [
+        {
+            "title": "Front End Developer Nano Degree",
+            "school": "Online",
+            "Year": "2016",
+            "url": "URL: Here"
+        }
+    ]
+};
+
+
+// Projects
+var projects = {
+    "projects": [{
+        "title": "Portfolio",
+        "dates": "2016",
+        "description": "Built a portfolio to showcase projects done so far.",
+        "images": ["images/projects/Featured_Work.png", "images/projects/Main_Portfolio.png", "images/projects/Project_Description.png"]
+
+    }]
+
+}
+
+
+
+
+bio.display = function() {
+    if (bio["skills"].length > 0) {
+
+        var formattedHTMLheaderName = HTMLheaderName.replace("%data%", bio.name);
+        var formattedHTMLheaderRole = HTMLheaderRole.replace("%data%", bio.role);
+        var formattedHTMLmobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+        var formattedHTMLemail = HTMLemail.replace("%data%", bio.contacts.email);
+        var formattedHTMLtwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+        var formattedHTMLgithub = HTMLgithub.replace("%data%", bio.contacts.github);
+        var formattedHTMLlocation = HTMLlocation.replace("%data%", bio.contacts.location);
+        var formattedHTMLpicture = HTMLbioPic.replace("%data%", bio.bioPic);
+        var formattedHTMLwelomeMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+
+        $("#header").prepend(formattedHTMLheaderRole);
+        $("#header").prepend(formattedHTMLheaderName);
+        $("#header").append(formattedHTMLpicture);
+        $("#header").append(formattedHTMLwelomeMessage);
+        $("#topContacts").append(formattedHTMLemail);
+        $("#topContacts").append(formattedHTMLgithub);
+        $("#topContacts").append(formattedHTMLlocation);
+    }
+}
+
+
+
+work.display = function() {
+    for (job in work.jobs) {
+        $("#workExperience").append(HTMLworkStart);
+
+        var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+        var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+        var formattedEmployerTitle = formattedEmployer + formattedTitle;
+        $(".work-entry:last").append(formattedEmployerTitle);
+
+        var formattedDate = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+        $(".work-entry:last").append(formattedDate);
+
+        var fomattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+        $(".work-entry:last").append(fomattedDescription);
+    }
+}
+
+projects.display = function() {
+
+    var formattedTitle = "";
+    var formattedDates = "";
+    var formattedDescription = "";
+    var formattedImage = "";
+
+    for (project in projects.projects) {
+
+        $("#projects").append(HTMLprojectStart);
+
+        formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+        formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+        formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+
+        $(".project-entry:last").append(formattedTitle);
+        $(".project-entry:last").append(formattedDates);
+        $(".project-entry:last").append(formattedDescription);
+
+        if (projects.projects[project].images.length > 0) {
+            for (image in projects.projects[project].images) {
+                formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+                $(".project-entry:last").append(formattedImage);
+            }
+        }
+    }
+}
+
+education.display = function() {
+
+
+    for (school in education.schools) {
+
+        $("#education").append(HTMLschoolStart);
+
+        var formattedHTMLschoolName = HTMLschoolName.replace("%data%", education.schools[school].name);
+        var formattedHTMLschoolDegree = HTMLschoolDegree.replace("%data%", education.schools[school].qualification);
+        var formattedHTMLschoolDates = HTMLschoolDates.replace("%data%", education.schools[school].year);
+        var formattedHTMLschoolLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+        var formattedHTMLschoolMajor = HTMLschoolMajor.replace("%data%", education.schools[school].major);
+        var formattedHTMLpicture = HTMLschoolPicture.replace("%data%", education.schools[school].schoolPic);
+
+        var finalFormattedHTMLeducation = formattedHTMLschoolName + formattedHTMLschoolDegree;
+        
+        
+        
+        
+        var HTMLschoolImageColumn = '<div class="col-xs-hidden col-sm-hidden col-md-1"></div>';
+        //var HTMLschoolInformationColumn = '<div class="col-xs-11 col-sm-11 col-md-11"></div>';
+        
+        //$(HTMLschoolInformationColumn)//.append(finalFormattedHTMLeducation, formattedHTMLschoolDates, formattedHTMLschoolLocation, formattedHTMLschoolMajor);
+
+        console.log(HTMLschoolInformationColumn);
+        $(HTMLrow).append("<p> Hello computer </p>" +  HTMLschoolInformationColumn);
+        
+        console.log(HTMLrow);
+
+
+        $(".education-entry:last").append(HTMLrow);
+        //$(HTMLrow).append(HTMLschoolImageColumn ,HTMLschoolInformationColumn, HTMLrow); 
+
+        //$("#school-img-col").append(formattedHTMLpicture);
+        // $("#school-info-col").append(finalFormattedHTMLeducation, formattedHTMLschoolDates, formattedHTMLschoolLocation, formattedHTMLschoolMajor);
+
+        //$(".education-entry:last").append(HTMLrow);// .append($(HTMLrow).append(HTMLschoolImageColumn, HTMLschoolInformationColumn));
+    }
+
+}
+
+bio.display();
+work.display();
+projects.display();
+education.display();
+
+
+
+
+$("#mapDiv").append(googleMap);
+
+
+
+
+
+// $("#internationlizeButton").click(inName($("#name").text()));
+
+
+// function inName(oldName)
+// {
+//     oldName = oldName.trim().split();
+
+//     console.log(finalName);
+//     var splitName = oldName.split(" ");
+
+//     console.log(oldName);
+
+//     finalName = "";
+
+//     for (var i = 0; i < splitName.length; i++)
+//     {
+//         finalName +=  i % 2 === 0 ? splitName[i].charAt(0).toUpperCase() + splitName[i].slice(1).toLowerCase() : splitName[i].toUpperCase() ;
+
+//         finalName += " ";
+//     }
+
+//     finalName = finalName.trim();
+
+//     return finalName;
+// }
