379182fffab4c48d02a7e67bfde2c92e9a0d69f9
Peter Manuel <peterjohn.manuel@gmail.com>
2016-12-04 02:03:23 +0200

    Logo's
    - Added more logo's
    - Added Skill class.
    - Added html and knockout binding to render logo's.

--- src/js/Models/Skills.js
+++ src/js/Models/Skills.js
@@ -1,6 +1,40 @@
-/** 
- * Skills
- */
+/**
+ * Skills 
+ * @class Skills
+ * @param img
+ * @param alt
+ * */
 
+function Skill(img, alternative) {
+    var self = this;
+    self.img = img;
+    self.alternative = alternative;
+}
 
-var allSkills = ['Front-End Desktop & Web Development', 'Back-End Development', 'C#.net', 'Windows Communication Foundation (WCF)', 'SQL Server'];
+var skill1 = new Skill('img/arduino_logo.png', 'Arduino');
+var skill2 = new Skill('img/bootstrap_logo.png', 'Bootstrap');
+var skill3 = new Skill('img/bower_logo.png', 'Bower');
+var skill4 = new Skill('img/csharp_logo.png', 'C#');
+var skill5 = new Skill('img/css3_logo.png', 'CSS3');
+var skill6 = new Skill('img/font-awesome_logo.png', 'Font Awesome');
+var skill7 = new Skill('img/git_logo.png', 'Git');
+var skill8 = new Skill('img/google_maps_logo.png', 'Google Maps');
+var skill9 = new Skill('img/grunt_logo.png', 'Grunt');
+var skill10 = new Skill('img/gulp_logo.png', 'Gulp');
+var skill11 = new Skill('img/html5_logo.png', 'Html5');
+var skill12 = new Skill('img/jasmine_logo.png', 'Jasmine');
+var skill13 = new Skill('img/javascript_logo.png', 'Javascript');
+var skill14 = new Skill('img/jquery_logo.png', 'Jquery');
+var skill15 = new Skill('img/knockout_logo.png', 'Knockout JS');
+var skill16 = new Skill('img/mysql_logo.png', 'My SQL');
+var skill17 = new Skill('img/php_logo.png', 'PHP');
+var skill18 = new Skill('img/raspberry_pi_logo.png', 'Raspberry Pi');
+var skill19 = new Skill('img/rwd_logo.png', 'Responsive Web Design');
+var skill20 = new Skill('img/sass_logo.png', 'Sass');
+var skill21 = new Skill('img/ubuntu_logo.png', 'Ubuntu');
+var skill22 = new Skill('img/visual_studio_logo.png', 'Visual Studio');
+
+var allSkills = [ skill1, skill2 , skill3, skill4, skill5, skill6, skill7, skill8, skill9, skill10, skill11, skill12, skill13, skill14, skill15, skill16 , skill17, skill8, skill19, 
+skill20, skill21, skill22
+     
+];
--- src/js/app.js
+++ src/js/app.js
@@ -13,7 +13,7 @@ ko.applyBindings(bi, document.getElementById('bio'));
 ko.applyBindings(occ, document.getElementById('occupation'));
 ko.applyBindings(edu, document.getElementById('education'));
 ko.applyBindings(topContact, document.getElementById('contact'));
-ko.applyBindings(ski, document.getElementById('skill'));
+ko.applyBindings(ski, document.getElementById('skill-set'));
 
 
 })();
