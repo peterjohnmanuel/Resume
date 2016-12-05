ef4c8e717113f0536fb0607f526796c65b5da3fe
Peter Manuel <peterjohn.manuel@gmail.com>
2016-12-05 19:39:43 +0200

    Fixed pi image that was showing as google maps.

--- src/js/Models/Skills.js
+++ src/js/Models/Skills.js
@@ -35,6 +35,6 @@ var skill21 = new Skill('img/ubuntu_logo.png', 'Ubuntu');
 var skill22 = new Skill('img/visual_studio_logo.png', 'Visual Studio');
 var skill23 = new Skill('img/windows_logo.png', 'Windows');
 
-var allSkills = [ skill1, skill2 , skill3, skill4, skill5, skill6, skill7, skill8, skill9, skill10, skill11, skill12, skill13, skill14, skill15, skill16 , skill17, skill8, skill19, 
+var allSkills = [ skill1, skill2 , skill3, skill4, skill5, skill6, skill7, skill8, skill9, skill10, skill11, skill12, skill13, skill14, skill15, skill16 , skill17, skill18, skill19, 
 skill20, skill21, skill22, skill23     
 ];
