de77cff9b28814474f50bbdc40a5dd15518be728
Peter John Manuel <peterjohn.manuel@gmail.com>
2017-02-15 12:27:02 +0200

    Updated certifications.

--- src/js/Models/Certifications.js
+++ src/js/Models/Certifications.js
@@ -2,15 +2,16 @@
  * @class Certification
  * @param description
  * @param year
-*/
+ */
 
 function Certification(certDescription, year) {
     var self = this;
-    self.certDescription = certDescription;    
+    self.certDescription = certDescription;
     self.year = year;
 }
 
 var certification1 = new Certification("Microsoft Certified Professional", '2016');
-var certification2 = new Certification("Microsoft C# Specialist", '2016');
+var certification2 = new Certification("Microsoft Specialist: Programming in C#", '2016');
+var certification3 = new Certification("Microsoft Specialist: Programming in HTML5 with JavaScript and CSS3", '2017');
 
-var allCertifications = [certification1, certification2];
+var allCertifications = [certification1, certification2, certification3];
