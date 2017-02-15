6f24c4e23bb7a971cac6d724075f7dc6aa37990a
Peter John Manuel <peterjohn.manuel@gmail.com>
2017-02-15 12:33:38 +0200

    Added Microsoft cert profile

--- src/js/Models/Contact.js
+++ src/js/Models/Contact.js
@@ -3,8 +3,8 @@
  * @param contactType
  * @param contactDetails
  * @param contactIcon
-*/
-function Contact(contactType, contactDetails ,contactIcon, isHyperlink){
+ */
+function Contact(contactType, contactDetails, contactIcon, isHyperlink) {
     var self = this;
 
     self.contactType = contactType;
@@ -18,7 +18,10 @@ var contact2 = new Contact('Skype', 'peterjohn.manuel', 'fa-skype', false);
 var contact3 = new Contact('Github', 'https://github.com/peterjohnmanuel', 'fa-github', true);
 var contact4 = new Contact('Location', 'Cape Town, RSA', 'fa-map-o');
 var contact5 = new Contact('Portfolio', 'http://manueloverride.ddns.net:8090/Portfolio', 'fa-briefcase', true);
+var contact6 = new Contact('Microsoft Profile', 'http://www.mycertprofile.com/Profile/6340282762', 'fa-windows', true);
 
 
 
-var allContacts = [contact1, contact2, contact3, contact4, contact5];
+
+
+var allContacts = [contact1, contact2, contact3, contact4, contact5, contact6];
