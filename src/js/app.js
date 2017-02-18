e6d6c24d83ff402c5c2b9708427a7fc559252e9a
Peter John Manuel <peterjohn.manuel@gmail.com>
2017-02-18 23:11:16 +0200

    Refactored header.

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,20 @@
-(function(){
+(function() {
 
-'use strict';
+    'use strict';
 
 
-var occ = new OccupationViewModel();
-var edu = new EducationViewModel();
-var topContact = new ContactViewModel();
-var ski = new SkillsViewModel();
-var bi = new BioViewModel();
-var cert = new CertificationViewModel();
+    var occ = new OccupationViewModel();
+    var edu = new EducationViewModel();
+    var topContact = new ContactViewModel();
+    var ski = new SkillsViewModel();
+    var bi = new BioViewModel();
+    var cert = new CertificationViewModel();
 
-ko.applyBindings(bi, document.getElementById('bio'));
-ko.applyBindings(occ, document.getElementById('occupation'));
-ko.applyBindings(edu, document.getElementById('education'));
-ko.applyBindings(topContact, document.getElementById('contact'));
-ko.applyBindings(ski, document.getElementById('skill-set'));
-ko.applyBindings(cert, document.getElementById('certfications'));
+    ko.applyBindings(bi, document.getElementById('bio'));
+    ko.applyBindings(occ, document.getElementById('occupation'));
+    ko.applyBindings(edu, document.getElementById('education'));
+    ko.applyBindings(topContact, document.getElementById('contacts'));
+    ko.applyBindings(ski, document.getElementById('skill-set'));
+    ko.applyBindings(cert, document.getElementById('certfications'));
 
 })();
