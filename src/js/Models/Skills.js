c308a68a000f54d24e7f29e508d22c9b44a4a539
Peter Manuel <peterjohn.manuel@gmail.com>
2017-02-20 08:54:43 +0200

    Inital Skillset
    - Added logos
    - Added Skills matrix.

--- src/js/Models/Skills.js
+++ src/js/Models/Skills.js
@@ -5,37 +5,322 @@
  * @param alt
  * */
 
-function Skill(img, alternative) {
+function Skill(img, alternative, skillLevel, skillColor) {
     var self = this;
     self.img = img;
     self.alternative = alternative;
+    self.skillLevel = skillLevel;
+    self.skillColor = skillColor;
 }
 
-var skill24 = new Skill('img/apache_logo.png', 'Apache');
-var skill1 = new Skill('img/arduino_logo.png', 'Arduino');
-var skill2 = new Skill('img/bootstrap_logo.png', 'Bootstrap');
-var skill3 = new Skill('img/bower_logo.png', 'Bower');
-var skill4 = new Skill('img/csharp_logo.png', 'C#');
-var skill5 = new Skill('img/css3_logo.png', 'CSS3');
-var skill6 = new Skill('img/font-awesome_logo.png', 'Font Awesome');
-var skill7 = new Skill('img/git_logo.png', 'Git');
-var skill8 = new Skill('img/google_maps_logo.png', 'Google Maps');
-var skill9 = new Skill('img/grunt_logo.png', 'Grunt');
-var skill10 = new Skill('img/gulp_logo.png', 'Gulp');
-var skill11 = new Skill('img/html5_logo.png', 'Html5');
-var skill12 = new Skill('img/jasmine_logo.png', 'Jasmine');
-var skill13 = new Skill('img/javascript_logo.png', 'Javascript');
-var skill14 = new Skill('img/jquery_logo.png', 'Jquery');
-var skill15 = new Skill('img/knockout_logo.png', 'Knockout JS');
-var skill16 = new Skill('img/mysql_logo.png', 'My SQL');
-var skill17 = new Skill('img/php_logo.png', 'PHP');
-var skill18 = new Skill('img/raspberry_pi_logo.png', 'Raspberry Pi');
-var skill19 = new Skill('img/rwd_logo.png', 'Responsive Web Design');
-var skill20 = new Skill('img/sass_logo.png', 'Sass');
-var skill21 = new Skill('img/ubuntu_logo.png', 'Ubuntu');
-var skill22 = new Skill('img/visual_studio_logo.png', 'Visual Studio');
-var skill23 = new Skill('img/windows_logo.png', 'Windows');
 
-var allSkills = [ skill24,skill1, skill2 , skill3, skill4, skill5, skill6, skill7, skill8, skill9, skill10, skill11, skill12, skill13, skill14, skill15, skill16 , skill17, skill18, skill19, 
-skill20, skill21, skill22, skill23     
-];
+// Build Management
+var BuildManagementSkill = function(img, alternative, skillLevel, skillColor) {
+    this.SkillType = 'Build Management';
+    Skill.call(this, img, alternative, skillLevel, skillColor);
+}
+
+BuildManagementSkill.prototype = Object.create(Skill.prototype);
+BuildManagementSkill.prototype.constructor = BuildManagementSkill;
+
+var bm1 = new BuildManagementSkill('img/grunt_logo.png', 'Grunt', 20, '#D35400');
+var bm2 = new BuildManagementSkill('img/gulp_logo.png', 'Gulp', 40, '#E74C3C');
+var bm3 = new BuildManagementSkill('img/msbuild_logo.png', 'MSBuild', 20, '#154360');
+
+// Collaboration
+var CollaborationSkill = function(img, alternative, skillLevel, skillColor) {
+    this.SkillType = 'Collaboration';
+    Skill.call(this, img, alternative, skillLevel, skillColor);
+}
+
+CollaborationSkill.prototype = Object.create(Skill.prototype);
+CollaborationSkill.prototype.constructor = CollaborationSkill;
+
+var cb1 = new CollaborationSkill('img/jira_logo.png', 'Jira', 40, '#2874A6');
+
+
+// Compiled Languages
+var CompiledLanguageSkill = function(img, alternative, skillLevel, skillColor) {
+    this.SkillType = 'Compiled Languages';
+    Skill.call(this, img, alternative, skillLevel, skillColor);
+}
+
+CompiledLanguageSkill.prototype = Object.create(Skill.prototype);
+CompiledLanguageSkill.prototype.constructor = CompiledLanguageSkill;
+
+var compiledSkill1 = new CompiledLanguageSkill('img/c++_logo.png', 'C++', 40, '#1F618D');
+var compiledSkill2 = new CompiledLanguageSkill('img/csharp_logo.png', 'C#', 60, '#6C3483');
+var compiledSkill3 = new CompiledLanguageSkill('img/java_logo.png', 'Java', 40, '#229954');
+var compiledSkill4 = new CompiledLanguageSkill('', 'Vb.net', 20, '#3498DB');
+
+// Continuos Integration
+var ContinuosIntegrationSkill = function(img, alternative, skillLevel, skillColor) {
+    this.SkillType = 'Continuos Integration';
+    Skill.call(this, img, alternative, skillLevel, skillColor);
+}
+
+ContinuosIntegrationSkill.prototype = Object.create(Skill.prototype);
+ContinuosIntegrationSkill.prototype.constructor = ContinuosIntegrationSkill;
+
+var ci1 = new ContinuosIntegrationSkill('img/bamboo_logo.png', 'Bamboo', 40, '#2874A6');
+
+// Collaboration
+
+
+// Design and Development
+var DesignDevelopmentSkill = function(img, alternative, skillLevel, skillColor) {
+    this.SkillType = 'Design and Development';
+    Skill.call(this, img, alternative, skillLevel, skillColor);
+}
+
+DesignDevelopmentSkill.prototype = Object.create(Skill.prototype);
+DesignDevelopmentSkill.prototype.constructor = DesignDevelopmentSkill;
+
+var dds1 = new DesignDevelopmentSkill('', 'Scrum', 60, '#D35400');
+var dds2 = new DesignDevelopmentSkill('', 'Client Server', 60, '#A93226');
+var dds3 = new DesignDevelopmentSkill('', 'Domain Driven Design (DDD)', 40, '#1ABC9C');
+var dds4 = new DesignDevelopmentSkill('', 'Object Orientation', 60, '#F4D03F');
+var dds5 = new DesignDevelopmentSkill('', 'Test Driven Development (TDD)', 20, '#85C1E9');
+var dds6 = new DesignDevelopmentSkill('', 'SOLID Principle', 60, '#7F8C8D');
+var dds7 = new DesignDevelopmentSkill('', 'Model View Controller (MVC)', 40, '#73C6B6');
+var dds8 = new DesignDevelopmentSkill('', 'Model View View Model (MVVM)', 40, '#E59866');
+var dds9 = new DesignDevelopmentSkill('', 'Unified Modeling Language', 60, '#884EA0');
+
+// Database 
+var DatabaseSkill = function(img, alternative, skillLevel, skillColor) {
+    this.SkillType = 'Database';
+    Skill.call(this, img, alternative, skillLevel, skillColor);
+}
+
+DatabaseSkill.prototype = Object.create(Skill.prototype);
+DatabaseSkill.prototype.constructor = DatabaseSkill;
+
+var databaseSkill1 = new DatabaseSkill('img/access_logo.png', 'MS Access', 20, '#E74C3C');
+var databaseSkill2 = new DatabaseSkill('img/mysql_logo.png', 'MySQL', 40, '#F39C12');
+var databaseSkill3 = new DatabaseSkill('img/oracle_logo.png', 'Oracle', 20, '#C0392B');
+var databaseSkill4 = new DatabaseSkill('img/sql-server_logo.png', 'SQL Server', 40, '#34495E');
+var databaseSkill5 = new DatabaseSkill('img/sybase_logo.png', 'Sybase ASE', 40, '#AAB7B8');
+
+
+// Frameworks
+var FrameworkSkill = function(img, alternative, skillLevel, skillColor) {
+    this.SkillType = 'Frameworks';
+    Skill.call(this, img, alternative, skillLevel, skillColor);
+}
+
+FrameworkSkill.prototype = Object.create(Skill.prototype);
+FrameworkSkill.prototype.constructor = FrameworkSkill;
+
+var fm1 = new FrameworkSkill('', '.net Framework', 60, '#2980B9');
+var fm2 = new FrameworkSkill('', 'ASP.NET', 40, '#9B59B6');
+var fm3 = new FrameworkSkill('', 'Entity Framework (EF)', 40, '#3498DB');
+var fm4 = new FrameworkSkill('', 'N-Hibernate', 40, '#2ECC71');
+var fm5 = new FrameworkSkill('img/telerik_logo.png', 'Telerik', 40, '#0E6655');
+var fm6 = new FrameworkSkill('', 'Windows Communication Foundation', 60, '#7F8C8D');
+
+
+// Operating Systems
+var OperatingSystemSkill = function(img, alternative, skillLevel, skillColor) {
+    this.SkillType = 'Operating Systems';
+    Skill.call(this, img, alternative, skillLevel, skillColor);
+}
+
+OperatingSystemSkill.prototype = Object.create(Skill.prototype);
+OperatingSystemSkill.prototype.constructor = OperatingSystemSkill;
+
+var osSkill1 = new OperatingSystemSkill('img/raspberry_pi_logo.png', 'Raspbian', 40, '#f472d0');
+var osSkill2 = new OperatingSystemSkill('img/ubuntu_logo.png', 'Ubuntu', 40, '#D35400  ');
+var osSkill3 = new OperatingSystemSkill('img/windows_7_logo.png', 'Windows 7', 80, '#C0392B');
+var osSkill4 = new OperatingSystemSkill('img/windows_logo.png', 'Windows 8 & 8.1', 60, '#8E44AD');
+var osSkill5 = new OperatingSystemSkill('img/windows_logo.png', 'Windows 10', 60, '#3498DB');
+var osSkill6 = new OperatingSystemSkill('img/windows_old_logo.png', 'Windows Server 2008', 40, '#E67E22');
+var osSkill7 = new OperatingSystemSkill('img/windows_logo.png', 'Windows Server 2012 & R2', 40, '#3498DB');
+
+// Source Control Management
+
+var SourceControlSkill = function(img, alternative, skillLevel, skillColor) {
+    this.SkillType = 'Source Control Management (SCM)';
+    Skill.call(this, img, alternative, skillLevel, skillColor);
+}
+
+SourceControlSkill.prototype = Object.create(Skill.prototype);
+SourceControlSkill.prototype.constructor = SourceControlSkill;
+
+var scm1 = new SourceControlSkill('img/github_logo.png', 'Github', 20, '#5F6A6A');
+var scm2 = new SourceControlSkill('img/git_logo.png', 'Git', 20, '#E67E22');
+var scm3 = new SourceControlSkill('img/gitlab_logo.png', 'GitLab', 20, '#D0D3D4');
+var scm4 = new SourceControlSkill('img/subversion_logo.png', 'Subversion', 60, '#B7950B');
+var scm5 = new SourceControlSkill('img/mercurial_logo.png', 'Mercurial', 40, '#839192');
+
+
+var TestingSkill = function(img, alternative, skillLevel, skillColor) {
+    this.SkillType = 'Testing';
+    Skill.call(this, img, alternative, skillLevel, skillColor);
+}
+
+TestingSkill.prototype = Object.create(Skill.prototype);
+TestingSkill.prototype.constructor = TestingSkill;
+
+var tes1 = new TestingSkill('img/jasmine_logo.png', 'Jasmine', 20, '#6C3483');
+
+
+// Web Development 
+var WebDevelopmentSkill = function(img, alternative, skillLevel, skillColor) {
+    this.SkillType = 'Web Development';
+    Skill.call(this, img, alternative, skillLevel, skillColor);
+}
+
+var webSkill1 = new WebDevelopmentSkill('img/bootstrap_logo.png', 'Bootstrap', 40, '#8E44AD');
+var webSkill2 = new WebDevelopmentSkill('img/css3_logo.png', 'CSS3', 40, '#3498DB');
+var webSkill3 = new WebDevelopmentSkill('img/html5_logo.png', 'Html5', 60, '#D35400');
+var webSkill4 = new WebDevelopmentSkill('img/javascript_logo.png', 'Javascript', 40, '#F4D03F');
+var webSkill5 = new WebDevelopmentSkill('img/jquery_logo.png', 'JQuery', 40, '#5499C7');
+var webSkill6 = new WebDevelopmentSkill('img/json_logo.png', 'JSON', 40, '#283747');
+var webSkill7 = new WebDevelopmentSkill('img/knockout_logo.png', 'Knockout JS', 20, '#A93226');
+var webSkill8 = new WebDevelopmentSkill('img/php_logo.png', 'PHP', 40, '#7D3C98');
+var webSkill9 = new WebDevelopmentSkill('img/sass_logo.png', 'Sass', 20, '#f472d0');
+var webSkill10 = new WebDevelopmentSkill('img/xml_logo.png', 'XML', 40, '#1C2833');
+
+WebDevelopmentSkill.prototype = Object.create(Skill.prototype);
+WebDevelopmentSkill.prototype.constructor = WebDevelopmentSkill;
+
+// Web Servers 
+var WebServerSkill = function(img, alternative, skillLevel, skillColor) {
+    this.SkillType = 'Operating Systems';
+    Skill.call(this, img, alternative, skillLevel, skillColor);
+}
+
+WebServerSkill.prototype = Object.create(Skill.prototype);
+WebServerSkill.prototype.constructor = WebServerSkill;
+
+var webServerSkill1 = new WebServerSkill('img/apache_logo.png', 'Apache', 40, '#E74C3C');
+var webServerSkill2 = new WebServerSkill('img/windows_logo.png', 'IIS', 60, '#2ECC71');
+
+
+var allSkills = [
+
+    {
+        name: 'Build Management',
+        items: [
+            bm1,
+            bm2,
+            bm3
+        ]
+    },
+
+    {
+        name: 'Collaboration',
+        items: [cb1]
+    },
+
+    {
+        name: 'Compiled Languages',
+        items: [
+            compiledSkill1,
+            compiledSkill2,
+            compiledSkill3,
+            compiledSkill4
+        ]
+    },
+
+    {
+        name: 'Continuos Integration',
+        items: [ci1]
+
+    },
+
+    {
+        name: 'Design and Development Approaches',
+        items: [
+            dds1,
+            dds2,
+            dds3,
+            dds4,
+            dds5,
+            dds6,
+            dds7,
+            dds8,
+            dds9
+        ]
+    },
+
+    {
+        name: 'Database',
+        items: [
+            databaseSkill1,
+            databaseSkill2,
+            databaseSkill3,
+            databaseSkill4,
+            databaseSkill5
+        ]
+    },
+
+    {
+        name: 'Framework',
+        items: [
+            fm1,
+            fm2,
+            fm3,
+            fm4,
+            fm5,
+            fm6
+        ]
+
+    },
+
+    {
+        name: 'Operating System',
+        items: [
+            osSkill1,
+            osSkill2,
+            osSkill3,
+            osSkill4,
+            osSkill5,
+            osSkill6,
+            osSkill7
+        ]
+    },
+
+    {
+        name: 'Source Control Management',
+        items: [
+            scm1,
+            scm2,
+            scm3,
+            scm4,
+            scm5
+        ]
+
+    },
+
+    {
+        name: 'Testing',
+        items: [tes1]
+    },
+
+
+    {
+        name: 'Web Development',
+        items: [
+            webSkill1,
+            webSkill2,
+            webSkill3,
+            webSkill4,
+            webSkill5,
+            webSkill6,
+            webSkill7,
+            webSkill8,
+            webSkill9
+
+        ]
+    },
+
+    {
+        name: 'Web Servers',
+        items: [
+            webServerSkill1,
+            webServerSkill2,
+        ]
+    },
+
+]
